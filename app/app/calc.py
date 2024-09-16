9723a6e065b44e01ee723b2b426a189fb5194b0e
codeninjaworld <praveen111890@gmail.com>
2024-09-17 00:50:22 +0530

    Added a new command for waiting database service is up or not

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+ Calculation function
+
+"""
+
+def add(x, y):
+    return x + y
+
+def substract(x, y):
+    return y - x
---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Sample Tests
+
+"""
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """ Testing the addition """
+
+    def test_add_numbers(self):
+        """ Test adding numbers together """
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_substract_numbers(self):
+        """ Test adding numbers together """
+        res = calc.substract(5, 6)
+
+        self.assertEqual(res, 1)
